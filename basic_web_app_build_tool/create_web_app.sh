4cb4fe3a8004c733a5d0c52c10ca66993915ec2a
Zachary Hughes <zachary.hughes@bright-wolf.com>
2017-04-06 20:30:41 -0400

    Added full path for easier web app creation

--- basic_web_app_build_tool/create_web_app.sh
+++ basic_web_app_build_tool/create_web_app.sh
@@ -2,14 +2,18 @@
 
 name="$1"
 
+#get name of directory this shell script lives in
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo $dir
+
 mkdir $name $name/js $name/css
 
-./create_html_template.sh > $name/index.html
+$dir/create_html_template.sh > $name/index.html
 chmod 755 $name/index.html
 
-./create_css_media_query_template.sh > $name/css/style.css
+$dir/create_css_media_query_template.sh > $name/css/style.css
 chmod 755 $name/css/style.css
 
 touch $name/js/script.js
 chmod 755 $name/js/script.js
-
